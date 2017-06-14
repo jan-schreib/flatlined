9036cc72b71e3e93be7153ddd5858290e9851d9b
Jan S <jan@oldbob.inter.net>
2017-06-14 21:49:25 +0200

    match all IPCMsgTypes, shorten the code, provide help menu

--- src/flatctl/bin/main.rs
+++ src/flatctl/bin/main.rs
@@ -1,19 +1,15 @@
 #[macro_use]
 extern crate log;
+extern crate clap;
 extern crate env_logger;
 extern crate ipc;
 
-use std::env;
-use std::process;
-
+use std::{process, fs, str};
+use clap::{Arg, App};
 use ipc::*;
 
 static FLATSOCK: &'static str = "ipc:///var/run/flatlined.sock";
-
-fn help() -> () {
-    println!("status|quit|none");
-    process::exit(1);
-}
+static FLATSOCKPATH: &'static str = "/var/run/flatlined.sock";
 
 fn communicate(ipc: &mut IPC, msg: IPCMsg) -> () {
 
@@ -36,29 +32,60 @@ fn communicate(ipc: &mut IPC, msg: IPCMsg) -> () {
             process::exit(1);
         }
     }
+
+    let response = str::from_utf8(&m.msg).unwrap();
     match m.typ {
-        IPCMsgType::Ok => println!("Ok received!"),
-        _ => println!("Shits received!"),
+        IPCMsgType::Ok => println!("{}", response),
+        IPCMsgType::Statistic => println!("{}", response),
+        IPCMsgType::Status => println!("{}", response),
+        IPCMsgType::Quit => println!("{}", response),
+        _ => println!("{}", response),
     };
-    println!("{}", std::str::from_utf8(&m.msg).unwrap());
 
     process::exit(0);
 }
 
 fn main() {
     env_logger::init().unwrap();
-    let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        help();
-    }
-    let mut ipc = IPC::new_connect(FLATSOCK);
+    let matches = App::new("flatctl - the tool for controlling the heartbeat daemon")
+        .version("0.1")
+        .arg(Arg::with_name("command")
+                 .short("c")
+                 .long("config")
+                 .value_name("COMMAND")
+                 .help("Valid commands: ok (default), status, statistic, quit, any")
+                 .takes_value(true))
+        .get_matches();
 
-    if args[1] == "status" {
-        let msg = IPCMsg {
-            typ: IPCMsgType::Status,
-            msg: [0u8; 1024],
-        };
-        communicate(&mut ipc, msg);
+    let mut ipc: IPC;
+    match fs::metadata(FLATSOCKPATH) {
+        Ok(_) => ipc = IPC::new_connect(FLATSOCK),
+        Err(_) => {
+            error!("IPC Socket not found.");
+            process::exit(1);
+        }
     }
+
+    let msg_type: IPCMsgType;
+    match matches.value_of("command") {
+        Some(x) => {
+            match x.as_ref() {
+                "ok" => msg_type = IPCMsgType::Ok,
+                "status" => msg_type = IPCMsgType::Status,
+                "statistic" => msg_type = IPCMsgType::Statistic,
+                "quit" => msg_type = IPCMsgType::Quit,
+                _ => msg_type = IPCMsgType::Any,
+            }
+        }
+        None => msg_type = IPCMsgType::Ok,
+    }
+
+    let mut msg = IPCMsg {
+        typ: msg_type,
+        msg: [0; 1024],
+    };
+
+    msg.create_payload(matches.value_of("command").unwrap()).unwrap();
+    communicate(&mut ipc, msg);
 }
