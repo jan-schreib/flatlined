2c70bf34da3145df3d0554498fb47c5bf7497f43
Jan S <jan.schreib@gmail.com>
2017-06-14 21:53:12 +0200

    provide impl for statistics

---
+++ src/flatlined/bin/stats.rs
@@ -0,0 +1,37 @@
+use server::Server;
+use std::fmt;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Statistic {
+    pub send_beats: u64,
+    pub recv_beats: u64,
+    pub server: Server,
+}
+
+impl fmt::Display for Statistic {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        try!(fmt.write_str(&self.send_beats.to_string()));
+        try!(fmt.write_str(" "));
+        try!(fmt.write_str(&self.recv_beats.to_string()));
+        try!(fmt.write_str(" "));
+        try!(fmt.write_str(&self.server.to_string()));
+        Ok(())
+    }
+}
+impl Statistic {
+    pub fn new(s: &Server) -> Statistic {
+        Statistic {
+            send_beats: 0,
+            recv_beats: 0,
+            server: s.clone(),
+        }
+    }
+
+    pub fn incr_send(&mut self) -> () {
+        self.send_beats += 1;
+    }
+
+    pub fn incr_recv(&mut self) -> () {
+        self.recv_beats += 1;
+    }
+}
